e5534f1689256189239b65da6b6caf990ac85712
nevin <aydingezernevin@gmail.com>
2022-04-10 09:47:49 +0300

    more work is committed

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -1,4 +1,11 @@
 package us100;
 
 public class TestCase001 {
+    public static void main(String[] args) {
+        System.out.println("Still working");
+
+        System.out.println("More work");
+
+
+    }
 }
---
+++ src/us100/TestCase002.java
@@ -0,0 +1,12 @@
+package us100;
+
+public class TestCase002 {
+    public static void main(String[] args) {
+        System.out.println("Practice makes perfect");
+
+
+        System.out.println("Practice makes progress");
+
+
+    }
+}
